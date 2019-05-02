7835c3fe53d71179c726f55ea870652c16b9d098
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2019-05-02 15:44:57 +0200

    Update

--- new-arfontsnet-scraper.py
+++ new-arfontsnet-scraper.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup as Soup
 from requests import get
-import re, sys, shutil, os
-from urllib.request import urlretrieve, build_opener, install_opener
+import re, os
+from urllib.request import urlretrieve
 from urllib.parse import quote
 
 BASE = "https://www.arfonts.net{}"
@@ -11,18 +11,6 @@ IMG_DL = BASE.format("/edits/textpreview.php?text=" +
                      quote("أبجد هوز حطي كلمن سعفص قرشت ثخذ ضظغ") +
                      "&font={}&ctx=000000&cbg=ffffff")
 IMG_DL.encode("utf-8")
-
-headers = [
-    ("User-Agent",
-     "Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0"),
-    ("Accept",
-     "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
-     ), ("Accept-Language", "en-US,ar;q=0.7,en;q=0.3"),
-    ("Accept-Encoding", "gzip, deflate, br"), ("Connection", "keep-alive"),
-    ("Upgrade-Insecure-Requests", "1")
-]
-
-_, headers = urlretrieve("https://www.arfonts.net", 'a.html')
 
 first_page = get(BASE.format(""))
 first_page.encoding = "utf-8"
@@ -58,9 +46,6 @@ for cat in categories:
                 os.makedirs('fonts/' + cat.text)
             fontname = f['onclick'][25:].strip("'")
             filename = "fonts/{0}/{1}".format(cat.text, fontname)
-            # opener = build_opener()
-            # opener.addheaders = [headers]
-            # install_opener(opener)
             _, headers = urlretrieve(FONT_DL.format(fontname),
                                      filename + '.zip')
             urlretrieve(IMG_DL.format(fontname), filename + '.png')
