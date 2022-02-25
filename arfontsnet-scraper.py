90758e1869f6252e2ca381889c48f86c9b69fb96
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-02-25 22:02:04 +0100

    Update script

--- arfontsnet-scraper.py
+++ arfontsnet-scraper.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from bs4 import BeautifulSoup as Soup
 from requests import get
 import re, os
@@ -7,24 +9,25 @@ from urllib.parse import quote
 
 BASE = "https://www.arfonts.net{}"
 BASE_PAGE = BASE + "&u={}"
-FONT_DL = "https://www.arfonts.net/fontfiles/zips/{}.zip"
+FONT_DL = "https://www.arfonts.net/fontfiles/zips/{}"
 
-IMG_DL = BASE.format("/edits/textpreview.php?text=" +
-                     quote("أبجد هوز حطي كلمن سعفص قرشت ثخذ ضظغ") +
-                     "&font={}&ctx=000000&cbg=ffffff")
+IMG_DL = BASE.format(
+    "/edits/textpreview.php?text="
+    + quote("أبجد هوز حطي كلمن سعفص قرشت ثخذ ضظغ")
+    + "&font={}&ctx=000000&cbg=ffffff"
+)
 IMG_DL.encode("utf-8")
 
 first_page = get(BASE.format(""))
 first_page.encoding = "utf-8"
 
+# print(first_page.text)
+
 first_page = Soup(first_page.text, features="lxml")
 
-sidebar = first_page.find('nav', id='mySidebar')
+sidebar = first_page.find("nav", id="mySidebar")
 
-categories = sidebar.find_all('a', {
-    'id': re.compile("^[a-z]{3}$"),
-    'href': re.compile('^/c:.*')
-})
+categories = sidebar.find_all("a", {"id": None, "href": re.compile("^/c:.*")})
 
 if not os.path.exists("fonts"):
     os.makedirs("fonts")
@@ -33,21 +36,20 @@ for cat in categories:
     have_fonts = True
     i = 1
     while have_fonts:
-        req = get(BASE_PAGE.format(cat['href'], i))
+        req = get(BASE_PAGE.format(cat["href"], i))
 
         req.encoding = "utf-8"
         page_bs = Soup(req.text, "lxml")
 
-        fonts = page_bs.find_all("button", class_='dbutton')
-        images = page_bs.find_all("button", class_='dbutton')
+        fonts = page_bs.find_all("a", {"rel": "nofollow", "href": re.compile("\\.zip$")})
 
         if len(fonts) == 0:
             have_fonts = False
 
         for f in fonts:
-            if not os.path.exists('fonts/' + cat.text):
-                os.makedirs('fonts/' + cat.text)
-            fontname = f['onclick'][25:].strip("'")
+            if not os.path.exists("fonts/" + cat.text):
+                os.makedirs("fonts/" + cat.text)
+            fontname = f["href"].strip("/").replace(":", "_")
             filename = "fonts/{0}/{1}".format(cat.text, fontname)
 
             # SPECIFIC MESSAGE:
@@ -60,15 +62,16 @@ for cat in categories:
             # The last time, I had to rewrite some parts of the script, but this time I added just 3 lines of code.
             # Thank you for making it easy for me, have a good day "Mr. X"!!
             opener = request.build_opener()
-            opener.addheaders = [(
-                'User-agent',
-                'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'
-            )]
+            opener.addheaders = [
+                (
+                    "User-agent",
+                    "Mozilla/5.0 (X11; Linux x86; rv:69.0) Gecko/20100108 Firefox/69.0",
+                )
+            ]
             request.install_opener(opener)
 
-            _, headers = urlretrieve(FONT_DL.format(fontname),
-                                     filename + '.zip')
-            urlretrieve(IMG_DL.format(fontname), filename + '.png')
+            _, headers = urlretrieve(FONT_DL.format(fontname), filename)
+            urlretrieve(IMG_DL.format(fontname[:-4]), filename[:-4] + ".png")
 
             print(fontname)
 
---
+++ extract.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+find fonts -name "*.zip" -execdir unzip -o {} ;
+find fonts -name "*.zip" -exec rm {} ;
